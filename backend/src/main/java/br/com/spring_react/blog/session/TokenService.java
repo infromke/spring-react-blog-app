60ca938d8412385a294be8258717d37eaaa86eb6
Inaros Lemuel <infromke@gmail.com>
2026-01-29 19:00:57 -0300

    create service layer for JWT generation and validation

---
+++ backend/src/main/java/br/com/spring_react/blog/session/TokenService.java
@@ -0,0 +1,52 @@
+package br.com.spring_react.blog.session;
+
+import br.com.spring_react.blog.user.internal.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.jwt.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.create()
+                    .withIssuer("blog-api")
+                    .withSubject(user.getId().toString())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Could not generate JSON token:", exception);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("blog-api")
+                    .build()
+                    .verify(token)
+                    .getSubject(); // retorna o ID do usuário
+        } catch (JWTVerificationException exception) {
+            return ""; // para token inválido ou expirado
+        }
+    }
+
+    private Instant genExpirationDate() {
+        // pega a hora atual, adiciona mais 2 horas e converte para UTC no fuso horário de Brasília
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
