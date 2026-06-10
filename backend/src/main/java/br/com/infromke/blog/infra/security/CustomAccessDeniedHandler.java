b97406d4de27217eb00c4a00e26285a53c422fc5
Inaros Lemuel <infromke@gmail.com>
2026-06-10 03:09:41 -0300

    feat: create CustomAuthenticationEntryPoint and CustomAccessDeniedHandler to intercept and delegate 401 and 403 errors

---
+++ backend/src/main/java/br/com/infromke/blog/infra/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,26 @@
+package br.com.infromke.blog.infra.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final HandlerExceptionResolver resolver;
+
+    public CustomAccessDeniedHandler(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
+        this.resolver = resolver;
+    }
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException ex) {
+        // deve encaminhar o erro para o GlobalExceptionHandler
+        resolver.resolveException(request, response, null, ex);
+    }
+}
---
+++ backend/src/main/java/br/com/infromke/blog/infra/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,26 @@
+package br.com.infromke.blog.infra.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final HandlerExceptionResolver resolver;
+
+    public CustomAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
+        this.resolver = resolver;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException ex) {
+        // deve encaminhar o erro para o GlobalExceptionHandler
+        resolver.resolveException(request, response, null, ex);
+    }
+}
